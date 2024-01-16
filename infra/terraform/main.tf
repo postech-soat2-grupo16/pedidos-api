341a27730cfb9e006979781600d18276737814c5
Pedro Haramoto <pedroharamoto@gmail.com>
2024-01-16 00:29:32 -0300

    test

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_lb_target_group" "tg_pedidos_api" {
     enabled             = true
     interval            = 30
     matcher             = "200-299"
-    path                = "/orders/healthcheck"
+    path                = "/orders/ping"
     port                = "traffic-port"
     protocol            = "HTTP"
     timeout             = 5
