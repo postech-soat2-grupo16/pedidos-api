53b037d4002b245780fdcb3348da314c6933eb78
Pedro Haramoto <pedroharamoto@gmail.com>
2024-01-27 21:21:24 -0300

    fix item

--- entities/orderedItem.go
+++ entities/orderedItem.go
@@ -1,7 +1,10 @@
 package entities
 
 type OrderedItem struct {
-	ItemID   string  `json:"item_id"`
-	Price    float64 `json:"price"`
-	Quantity int     `json:"quantity"`
+	ItemID      string  `json:"item_id"`
+	Price       float64 `json:"price"`
+	Quantity    int     `json:"quantity"`
+	Name        string  `json:"name"`
+	Category    string  `json:"category"`
+	Description string  `json:"description"`
 }
